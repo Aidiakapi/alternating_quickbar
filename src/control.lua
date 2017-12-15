ac55ac46fe3a94e2c883a393ae0b08171c45809b
Aidiakapi <aidiakapi@gmail.com>
2017-12-15 17:43:27 +0100

    Update to use swap_stack and properly handle in-cursor items.

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,52 @@
-local load_swap_logic = require('swap_logic')
+local QUICKBAR_SIZE = 10
+
+-- I still consider this rediculousness a bug, but whatever...
+-- https://forums.factorio.com/viewtopic.php?t=54895
+local function swap_stack(a, b)
+    if a.valid_for_read or b.valid_for_read then
+        a.swap_stack(b)
+    else
+        -- Assumes nobody removed iron-plate, if they did
+        -- they'd also break the default scenario so...
+        a.set_stack({ name = 'iron-plate', count = 1 })
+        a.swap_stack(b)
+        b.set_stack(nil)
+    end
+end
+
+local function load_swap_logic(player)
+    local quickbar = player.get_quickbar()
+
+    if quickbar == nil then return 0 end
+
+    local row_count = #quickbar / QUICKBAR_SIZE
+    return row_count, function (a, b)
+        if a == b then return end
+
+        a = (a - 1) * QUICKBAR_SIZE + 1
+        b = (b - 1) * QUICKBAR_SIZE + 1
+
+        -- Reset active filters
+        local filters = {}
+        for i = 1, QUICKBAR_SIZE do
+            filters[i] = quickbar.get_filter(a + i - 1)
+            filters[i + QUICKBAR_SIZE] = quickbar.get_filter(b + i - 1)
+            quickbar.set_filter(a + i - 1, nil)
+            quickbar.set_filter(b + i - 1, nil)
+        end
+        
+        -- Swap the slots
+        for i = 0, QUICKBAR_SIZE - 1 do
+            swap_stack(quickbar[a + i], quickbar[b + i])
+        end
+
+        -- Restore active filters
+        for i = 1, QUICKBAR_SIZE do
+            quickbar.set_filter(a + i - 1, filters[i + QUICKBAR_SIZE])
+            quickbar.set_filter(b + i - 1, filters[i])
+        end
+    end
+end
 
 script.on_init(function ()
     script.on_event(defines.events.on_tick, function ()
@@ -51,6 +99,9 @@ script.on_event('aq_quickbar_rotate', function (event)
     local quickbar_count, swap_quickbar = load_swap_logic(player)
     if quickbar_count == 0 then
         player.print('No quickbar found on player')
+        return
+    end
+    if quickbar_count == 1 then
         return
     end
 
